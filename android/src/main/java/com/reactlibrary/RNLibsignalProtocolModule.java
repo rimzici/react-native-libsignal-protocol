56f632c75c16e525c140bfa4287721f0474b8f3c
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2019-08-12 14:27:29 +0530

    Add first native method that is exposed to JS
    Adding method 'generateIdentityKeyPair', which has a callback
    function, called with resultant public and private key.

--- android/src/main/java/com/reactlibrary/RNLibsignalProtocolModule.java
+++ android/src/main/java/com/reactlibrary/RNLibsignalProtocolModule.java
@@ -5,6 +5,13 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import org.whispersystems.libsignal.IdentityKeyPair;
+import org.whispersystems.libsignal.util.KeyHelper;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Arguments;
+
+import android.util.Log;
+import android.util.Base64;
 
 public class RNLibsignalProtocolModule extends ReactContextBaseJavaModule {
 
@@ -18,5 +25,16 @@ public class RNLibsignalProtocolModule extends ReactContextBaseJavaModule {
   @Override
   public String getName() {
     return "RNLibsignalProtocol";
+  }
+
+  @ReactMethod
+  public void generateIdentityKeyPair(Callback successCallback) {
+    IdentityKeyPair identityKeyPair = KeyHelper.generateIdentityKeyPair();
+    String publicKey = Base64.encodeToString(identityKeyPair.getPublicKey().serialize(), Base64.NO_WRAP);
+    String privateKey = Base64.encodeToString(identityKeyPair.getPrivateKey().serialize(), Base64.NO_WRAP);
+    WritableMap keyPairMap = Arguments.createMap();
+    keyPairMap.putString("publicKey", publicKey);
+    keyPairMap.putString("privateKey", privateKey);
+    successCallback.invoke(keyPairMap);
   }
 }
