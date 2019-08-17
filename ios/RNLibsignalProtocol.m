9f719f5782ebba7f8a57d9faf9fd631bf4edf6a4
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2019-08-18 00:01:29 +0530

    Disable SignalProtocol in iOS completely

--- ios/RNLibsignalProtocol.m
+++ ios/RNLibsignalProtocol.m
@@ -1,8 +1,3 @@
 
 #import <React/RCTBridgeModule.h>
 
-@interface RCT_EXTERN_MODULE(RNLibsignalProtocol, NSObject)
-RCT_EXTERN_METHOD(generateIdentityKeyPair: (RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(testCall: (NSString *)value)
-@end
-  
--- ios/RNLibsignalProtocol.swift
+++ ios/RNLibsignalProtocol.swift
@@ -7,25 +7,3 @@
 //
 
 import Foundation
-import SignalProtocol
-
-@objc(RNLibsignalProtocol)
-class RNLibsignalProtocol: NSObject {
-
-    @objc func generateIdentityKeyPair(_ resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
-        if let data = try? SignalCrypto.generateIdentityKeyPair() {
-            print("TEST data", data);
-            // print("TEST data.base64EncodedString", data.base64EncodedString());
-            // resolve(data.base64EncodedString());
-        } else {
-            reject("RNLibsignal Error", "Cannot generate key pair", nil);
-        }
-    }
-
-    @objc func testCall(_ value: String) {
-        print("TEST value", value);
-        // var data = Data(base64Encoded: value)!
-        // print("TEST value data", data);
-        // print("TEST value.base64EncodedString", data.base64EncodedString());
-    }
-}
